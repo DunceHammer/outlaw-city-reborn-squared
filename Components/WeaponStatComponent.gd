63c3dff78fecaf62c8c7521e8a9a0c41672fe652
dominator <you@example.com>
2024-01-31 09:04:26 +0100

    Fix style

--- Components/WeaponStatComponent.gd
+++ Components/WeaponStatComponent.gd
@@ -9,9 +9,3 @@ class_name WeaponStatComponent
 @export var weapon_position : Vector3 = Vector3(0, 0, 0)
 @export var weapon_rotation : Vector3 = Vector3(0, 0, 0)
 @export var attack_cooldown : float = 1.0
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
--- Prefabs/TrainingDummy/TrainingDummy.gd
+++ Prefabs/TrainingDummy/TrainingDummy.gd
@@ -2,13 +2,13 @@ extends Node3D
 
 
 func _ready():
-	$HealthComponent.connect("damaged", on_damaged, 0)
-	$HealthComponent.connect("died", on_died, 0)
+	$HealthComponent.damaged.connect(_on_damaged,0)
+	$HealthComponent.died.connect(_on_died,0)
 
 
-func on_damaged(_damage: int):
+func _on_damaged(_damage: int):
 	print("Health: " + str($HealthComponent.health) + "/" + str($HealthComponent.max_health))
 
 
-func on_died():
+func _on_died():
 	queue_free()
